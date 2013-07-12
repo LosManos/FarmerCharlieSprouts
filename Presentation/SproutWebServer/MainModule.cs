faa032eb707e6227abd826496e508f0886db58e1
LosManos <ola.fjelddahl@gmail.com>
2013-07-12 23:18:12 +0200

    0.1.3 - Added Nancy with Razor.

---
+++ Presentation/SproutWebServer/MainModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutWebServer
+{
+	public class MyModel
+	{
+		public string Name { get; set; }
+	}
+
+	public class MainModule : Nancy.NancyModule
+	{
+		public MainModule()
+		{
+			Get["/"] = x =>
+				{
+					//return "hello world";
+					return View["Index", new MyModel{Name="from model"}];
+				};
+		}
+	}
+}
---
+++ Presentation/SproutWebServer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SproutWebServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var host = new Nancy.Hosting.Self.NancyHost(
+				new Uri("http://localhost:666")
+				))
+			{
+				host.Start();
+
+				Console.Write("Press any key");
+				Console.ReadKey();
+				host.Stop();
+			}
+		}
+	}
+}
