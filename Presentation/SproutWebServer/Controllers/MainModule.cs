6cb887a0a4f524d29f6f7908e322a6f21a12659f
LosManos <ola.fjelddahl@gmail.com>
2013-07-12 23:27:50 +0200

    Moved Module to Controllers folder.

--- Presentation/SproutWebServer/Controllers/MainModule.cs
+++ Presentation/SproutWebServer/Controllers/MainModule.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SproutWebServer
+namespace SproutWebServer.Controllers
 {
 	public class MainModule : Nancy.NancyModule
 	{
